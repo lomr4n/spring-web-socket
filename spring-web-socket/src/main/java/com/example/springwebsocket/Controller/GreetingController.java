55e6ad22061465df853679d3276b648003a09933
Javier Mármol <javier.marmol@bosonit.com>
2021-09-30 16:56:02 +0200

    first commit

---
+++ spring-web-socket/src/main/java/com/example/springwebsocket/Controller/GreetingController.java
@@ -0,0 +1,18 @@
+package com.example.springwebsocket.Controller;
+
+import com.example.springwebsocket.Domain.Greeting;
+import com.example.springwebsocket.Domain.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greet(HelloMessage message) throws InterruptedException {
+        Thread.sleep(1000);
+        return new Greeting("Hello" + HtmlUtils.htmlEscape(message.getName()));
+    }
+}
---
+++ spring-web-socket/src/main/java/com/example/springwebsocket/Domain/HelloMessage.java
@@ -0,0 +1,13 @@
+package com.example.springwebsocket.Domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+public class HelloMessage {
+
+    public String name;
+}
